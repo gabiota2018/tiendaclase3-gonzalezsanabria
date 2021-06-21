97c25cf0abce7ef74ef436d4969419b1d65ab0ca
gabiota2018 <gabisago6@gmail.com>
2021-06-21 17:45:38 -0300

    primer comit

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+   return(
+    <img 
+     src="https://st2.depositphotos.com/3665639/7453/v/600/depositphotos_74537483-stock-illustration-shopping-cart-icon.jpg"
+	 width='100'
+    />
+  );
+  
+} 
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function NavBar() {
+  return (
+    <div style={{width:'100%',height:'50px', background:'rgb(97,218,215)'}}>
+      <a href="#">Productos</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  <a href="#">Ofertas</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  <a href="#">Pedidos</a>
+    </div>
+  )
+}
+
+export default NavBar;
